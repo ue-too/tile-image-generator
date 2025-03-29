56b9ded724ac19b87ccdafb88c258e53a2536ec1
Vincent Chang <vntchang@gmail.com>
2025-03-29 16:30:12 +0800

    Fix Tailwind content configuration to include all necessary files

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx,css}",
+    "./src/App.tsx",
+    "./src/TileGenerator.tsx",
+    "./src/App.css",
+    "./src/index.css"
   ],
   theme: {
     extend: {},
